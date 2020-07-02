06ccd6d94a429ce817e248e3ecc166e644a1eae6
vincent <vincent.roquin@gmail.com>
2020-07-02 18:02:45 +0200

    add plop page

--- plop.php
+++ plop.php
@@ -9,23 +9,22 @@ if (file_exists(__DIR__ . '/.env')) {
     $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
     $dotenv->load();
 }
+echo('hello');
 try{
     $host = $_ENV['DB_HOST'];
     $dbname = $_ENV['DB_NAME'];
     $user = $_ENV['DB_USER'];
-    $pass = $_ENV['DB_PASSWORD'];        
+    $pass = $_ENV['DB_PASSWORD'];     
 
     $bdd = new \PDO(
         "mysql:host=".$host.";dbname=".$dbname.";charset=utf8", 
         $user, 
         $pass
     );
-
-
-    return $bdd; 
 }catch(Exception $e){
     die("Error: " .$e->getMessage());
 }
+
 $tests = $bdd->prepare('SELECT * FROM test');
 $tests->execute(array());
 $data = $tests->fetchAll();
